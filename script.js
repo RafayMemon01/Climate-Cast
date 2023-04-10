7e35120fc737a5f96416ed4a9ac7f951aa179019
Rafay Memon <119926891+RafayMemon01@users.noreply.github.com>
2023-04-10 17:23:51 +0500

    update

--- script.js
+++ script.js
@@ -14,27 +14,24 @@ window.getWheather = function () {
       console.log(response.data);
       let heading = document.querySelector(".modalHeading").innerHTML = `Climate Of ${cityName}`;
       let temp = document.querySelector("#temp").innerHTML = `${response.data.main.temp}`;
+
+      let feelsLike = document.querySelector(".feelsLike").innerHTML = `${response.data.main.temp}`;
+      let minTemp = document.querySelector("#minTemp").innerHTML = `${response.data.main.temp}`;
     })
     .catch(function (error) {
       // handle error
       console.log(error);
+      let errorResult = document.querySelector("#result").innerHTML=`${cityName} is not a city Name `
       r1 = document.querySelector('.no-display').classList.toggle('no-display');
     });
-    // Also, there is an error in the catch method, where you should pass error instead of error.data to the console.log method. Additionally, the classList.toggle() method should be called on the r1 object, not on document.querySelector('.no-display').
-    
-    // Finally, you may want to check if the response status is not a success (e.g., 404), and handle it accordingly.
-    
-    
-    
-    
-    
-    
-    
-    let heading = document.querySelector(".modalHeading").innerHTML = `Climate Of ${cityName}`;
-    
-    let temp = document.querySelector("#temp").innerHTML = `${response.data.main.temp}`;
-    let feelsLike = document.querySelector(".feelsLike").innerHTML = `${response.data.main.temp}`;
-    let minTemp = document.querySelector("#minTemp").innerHTML =  `${response.data.main.temp}`;
+
+
+
+
+
+
+
+
 
 
 
